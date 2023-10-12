b5a0d08863f3c89009af6cbac3e498c7738241ff
gus1150 <110936812+gus1150@users.noreply.github.com>
2023-10-12 12:53:05 +0900

    🚧 toggling bookmark button

--- js/order-cta.js
+++ js/order-cta.js
@@ -18,3 +18,27 @@ function closeOderModal() {
   orderModalOverlay.classList.remove('is-active')
 }
 orderModalOverlay.addEventListener('click', closeOderModal)
+
+function toggleOrderCtaBookmark() {
+  const [icon, countSpan] = this.children //북마크의 자식 [아이콘, 카운트 span]
+  const count = Number(countSpan.innerHTML.replaceAll(',', ''))
+
+  let newCount = count
+
+  if (this.classList.contains('is-active')) {
+    //활성화가 된 상태이니 -> 비활성화(ic-bookmark) -> 숫자도 -1
+    icon.classList.add('ic-bookmark')
+    icon.classList.remove('ic-bookmark-filled')
+    newCount = newCount - 1
+  } else {
+    //비활성화가 된 상태이니 -> 활성화(ic-bookmark-filled) -> 숫자도 +1
+    icon.classList.add('ic-bookmark-filled')
+    icon.classList.remove('ic-bookmark')
+    newCount = newCount + 1
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString()
+
+  this.classList.toggle('is-active')
+}
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
