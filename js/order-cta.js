93e4d43283b44f9629d655f230733d80c2f2a235
gus1150 <110936812+gus1150@users.noreply.github.com>
2023-10-18 10:38:22 +0900

    🚸 Toggle aria-label text of bookmark-button

--- js/order-cta.js
+++ js/order-cta.js
@@ -38,6 +38,7 @@ function toggleOrderCtaBookmark() {
   }
 
   countSpan.innerHTML = newCount.toLocaleString()
+  countSpan.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`)
 
   this.classList.toggle('is-active')
 }
